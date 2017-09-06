5a1d1a6aa8f519806414ea0a206c5bafc696be35
Lei Jie <839054164@qq.com>
2017-09-06 12:02:24 +0800

    add moving monsters

--- js/app.js
+++ js/app.js
@@ -6,59 +6,37 @@ window.requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAn
 
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-// var _canvasBuffer.setAttribute('height', canvas.height);
-// var _canvasBufferContext = _canvasBuffer.getContext('2d');
-
-/*
-beginPath()
-新建一条路径，生成之后，图形绘制命令被指向到路径上生成路径。
-closePath()
-闭合路径之后图形绘制命令又重新指向到上下文中。
-stroke()
-通过线条来绘制图形轮廓。
-fill()
-通过填充路径的内容区域生成实心的图形。
-
-fillRect(x, y, width, height)
-绘制一个填充的矩形
-strokeRect(x, y, width, height)
-绘制一个矩形的边框
-clearRect(x, y, width, height)
-清除指定矩形区域，让清除部分完全透明。
-
-*/
 
 var x = 325;
 var y = 525;
 var w = 50;
 var h = 50;
-var step = 30;
-var steps = 15;
 var bullets = [];
-var plane
-	//var bullet = new Bullet();
-var clearShoot;
-var interval = 100;
-ctx.fillStyle = "rgb(200,0,0)";
+var monsters = [];
+var monstersNumber = 7;
+var monsterDirection = 'right';
+var monsterDown = false;
 
+ctx.fillStyle = "rgb(200,0,0)";
 ctx.beginPath();
 ctx.fillRect(x, y, w, h);
 ctx.lineWidth = 2;
 
-function Bullet() {
+function Bullet(x, y, size, speed) {
 	this.x = x;
 	this.y = y;
-	this.size = 10;
+	this.size = size;
+	this.speed = speed
 }
 
 Bullet.prototype.fly = function() {
 	this.x += 0;
-	this.y -= 10;
+	this.y -= this.speed;
 	return this;
 }
 
 Bullet.prototype.draw = function() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
 	ctx.beginPath();
 	ctx.strokeStyle = '#fff';
 	ctx.moveTo(this.x + w / 2, this.y);
@@ -67,65 +45,206 @@ Bullet.prototype.draw = function() {
 	return this;
 }
 
-function Plane() {
+function Plane(x, y, w, h, speed) {
 	this.x = x;
 	this.y = y;
+	this.w = w;
+	this.h = h;
+	this.speed = speed
+
 }
 
-Plane.prototype.fly() {
+Plane.prototype.fly = function() {
 
 }
-Plane.prototype.draw() {
+Plane.prototype.draw = function() {
+	ctx.fillRect(this.x, this.y, this.w, this.h);
+}
 
+function Monster(x, y, speed, size) {
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
+	this.size = size;
+}
+Monster.prototype.fly = function(len) {
+	//为什么传入的len有6？
+	//console.log(len);
+	if (this.y > 500) {
+		monsters = []; //删除
+	}
+	if (monsterDirection == 'right') {
+		this.x += this.speed;
+	} else if (monsterDirection == 'left') {
+		this.x -= this.speed;
+	}
+
+	if (this.x > (670 - this.size - 26)) {
+		//console.log(monsters[0])
+		monsterDirection = 'left';
+		monsterDown = true;
+		//this.x += this.speed;
+	} else if (this.x < 28 + this.size) {
+		var l = monstersNumber - 1;
+		while (l--) {
+			monsters[l].x -= 2 * this.speed;
+		}
+		//console.log(this, len)
+		//console.log(monsters)
+		monsterDirection = 'right';
+		monsterDown = true;
+	}
+
+	if (monsterDown) {
+		var l = monstersNumber;
+		while (l--) {
+			monsters[l].y += 50;
+		}
+		monsterDown = false;
+	}
+
+
+}
+
+Monster.prototype.draw = function() {
+	ctx.fillRect(this.x, this.y, this.size, this.size);
+
+}
+
+function monsterInit() {
+	var len = monstersNumber;
+	while (len--) {
+		var a = 76,
+			b = 0,
+			c = 2,
+			d = 50;
+		var m = new Monster(a + len * 60, b, c, d);
+		monsters.push(m);
+	}
+}
+monsterInit();
+
+function updateMonsters() {
+
+	var len = monsters.length;
+	while (len--) {
+		var m = monsters[len];
+		m.fly(len);
+		m.draw();
+	}
+}
+
+function keyBoard() {
+	//console.log(this.keydown)
+	window.onkeydown = this.keydown.bind(this);
+	window.onkeyup = this.keyup.bind(this);
 }
 
 keyBoard.prototype = {
 	pressedLeft: false,
 	pressedRight: false,
 	pressedUp: false,
+	pressedDown: false,
 	pressedSpace: false,
-	keydown: function() {
-
+	pressedSpaceHeld: false,
+	pressedSpaceUp: true,
+	keydown: function(e) {
+		switch (e.keyCode) {
+			case 37:
+				this.pressedLeft = true;
+				//console.log("pressedLeft")
+				break;
+			case 38:
+				this.pressedUp = true;
+				break;
+			case 39:
+				this.pressedRight = true;
+				break;
+			case 40:
+				this.pressedDown = true;
+				break;
+			case 32:
+				this.pressedSpace = true;
+				this.pressedSpaceUp = false;
+				break;
+		}
+	},
+	keyup: function(e) {
+		switch (e.keyCode) {
+			case 37:
+				this.pressedLeft = false;
+				break;
+			case 38:
+				this.pressedUp = false;
+				break;
+			case 39:
+				this.pressedRight = false;
+				break;
+			case 40:
+				this.pressedDown = false;
+				break;
+			case 32:
+				this.pressedSpace = false;
+				this.pressedSpaceHeld = false;
+				this.pressedSpaceUp = true;
+				//console.log('up')
+				break;
+		}
 	}
 }
 
-function keyBoard(e) {
-
-
-
-	if (e.keyCode == 37) {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.beginPath();
-		x -= steps;
-		ctx.fillRect(x, y, w, h);
+// 
+function updateKeyBoard() {
+	if (keyboard.pressedLeft) {
+		plane.x -= plane.speed
 	}
-	if (e.keyCode == 38) {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.beginPath();
-		y -= steps;
-		ctx.fillRect(x, y, w, h);
+	if (keyboard.pressedRight) {
+		plane.x += plane.speed
 	}
-	if (e.keyCode == 39) {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.beginPath();
-		x += steps;
-		ctx.fillRect(x, y, w, h);
+	if (keyboard.pressedUp) {
+		plane.y -= plane.speed
 	}
-	if (e.keyCode == 40) {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.beginPath();
-		y += steps;
-		ctx.fillRect(x, y, w, h);
+	if (keyboard.pressedDown) {
+		plane.y += plane.speed
 	}
-	if (e.keyCode == 32) {
-		var bullet = new Bullet();
-		//console.log(bullet)
+	//长按空格子弹连射
+	if (keyboard.pressedSpaceHeld) {
+		var bullet = new Bullet(plane.x, plane.y, 10, 10);
 		bullets.push(bullet);
-		console.log(bullets);
 	}
+	//单按空格子弹单射
+	else if (keyboard.pressedSpace) {
+		setTimeout(() => {
+			if (keyboard.pressedSpace) {
+				keyboard.pressedSpaceHeld = true;
+			} else {
+				keyboard.pressedSpaceHeld = false;
+			}
+		}, 1000 / 30);
+		keyboard.pressedSpace = false;
+		var bullet = new Bullet(plane.x, plane.y, 10, 10);
+		bullets.push(bullet);
+
+	}
+	/*
+	//先持续移动再发射子弹
+	// else if ((keyboard.pressedLeft || keyboard.pressedRight || keyboard.pressedUp || keyboard.pressedDown) && !keyboard.pressedSpaceUp && !keyboard.pressedSpace) {
+	// 	setTimeout(() => {
+	// 		if (!keyboard.pressedSpaceUp) {
+	// 			keyboard.pressedSpaceHeld = true;
+	// 		} else {
+	// 			keyboard.pressedSpaceHeld = false;
+	// 		}
+	// 	}, 1000 / 30);
+	// }
+	*/
+
 }
 
-function update() {
+var plane = new Plane(325, 525, 50, 50, 5);
+var keyboard = new keyBoard();
+
+function updateBullets() {
 	var len = bullets.length;
 	while (len--) {
 		var b = bullets[len];
@@ -133,9 +252,19 @@ function update() {
 		if (b.y <= 0) bullets.splice(len, 1);
 		b.draw();
 	}
+}
+
+
+
+function update() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	updateKeyBoard();
+	updateMonsters();
+	updateBullets();
+	plane.draw();
+
 	requestAnimFrame(function() {
 		update()
 	})
 }
 update();
-window.onkeydown = keyBoard
